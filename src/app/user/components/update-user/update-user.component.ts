14df6f3758935790c0f23f6a48d6086ec945a37d
Khung_Long_Xanh <giangnguyenhhp@gmail.com>
2022-11-27 15:40:21 +0700

    update NgxPermission

---
+++ src/app/user/components/update-user/update-user.component.ts
@@ -0,0 +1,69 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {FormControl, FormGroup} from "@angular/forms";
+import {Role} from "../../../role/models/role";
+import {RoleService} from 'src/app/role/service/role.service';
+import {UserService} from '../../service/user.service';
+import {UpdateUserRequest} from "../../model/UpdateUserRequest";
+
+@Component({
+  selector: 'app-update-user',
+  templateUrl: './update-user.component.html',
+  styleUrls: ['./update-user.component.scss']
+})
+export class UpdateUserComponent implements OnInit {
+
+  updateUserFormGroup = new FormGroup({
+    userName: new FormControl(),
+    phoneNumber: new FormControl(),
+    email: new FormControl(),
+    roles: new FormControl(),
+    id: new FormControl(),
+  });
+  roles: Role[] = [];
+  userRoles: string[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<UpdateUserComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private roleService: RoleService,
+    private userService: UserService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.initFormData()
+    this.getRolesByUser()
+    this.getAllRoles()
+
+  }
+
+  private initFormData() {
+    this.updateUserFormGroup.patchValue(this.data)
+  }
+
+  submit() {
+    const request = <UpdateUserRequest>this.updateUserFormGroup.value
+    this.userService.updateUser(request).subscribe(response => {
+      if (response) {
+        this.dialogRef.close(true)
+      }
+    })
+  }
+
+  private getRolesByUser() {
+    this.userService.getRolesByUserId(this.data.id).subscribe((res: any) => {
+      this.userRoles = res;
+      this.updateUserFormGroup.controls.roles.setValue(this.userRoles)
+      console.log(this.userRoles)
+    })
+  }
+
+  private getAllRoles() {
+    this.roleService.getAllRoles().subscribe(r => {
+      if (r) {
+        this.roles = r
+      }
+    })
+  }
+}
---
+++ src/app/user/model/UpdateUserRequest.ts
@@ -0,0 +1,7 @@
+export interface UpdateUserRequest {
+  "userName": string
+  "email": string
+  "phoneNumber": string
+  "roles": [string]
+  id: string
+}
